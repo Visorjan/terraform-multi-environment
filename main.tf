329cf2c69838a93965533a28abcc053225b0d66d
Visorjan Koja <visorjan.koja@dci-student.org>
2025-01-27 00:04:55 +0100

    Aditional commit, to fix tags

--- main.tf
+++ main.tf
@@ -6,11 +6,19 @@ resource "aws_vpc" "dev" {
   cidr_block           = var.dev_vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
+
+  tags = {
+    Name = "dev_vpc"
+  }
 }
 
 resource "aws_subnet" "dev_subnet" {
   vpc_id     = aws_vpc.dev.id
   cidr_block = var.dev_subnet_cidr
+
+  tags = {
+    Name = "dev_subnet"
+  }
 }
 
 resource "aws_instance" "dev_instance" {
@@ -27,11 +35,19 @@ resource "aws_vpc" "staging" {
   cidr_block           = var.staging_vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
+
+  tags = {
+    Name = "staging_vpc"
+  }
 }
 
 resource "aws_subnet" "staging_subnet" {
   vpc_id     = aws_vpc.staging.id
   cidr_block = var.staging_subnet_cidr
+
+  tags = {
+    Name = "staging_subnet"
+  }
 }
 
 resource "aws_instance" "staging_instance" {
@@ -48,11 +64,19 @@ resource "aws_vpc" "prod" {
   cidr_block           = var.prod_vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
+
+  tags = {
+    Name = "prod_vpc"
+  }
 }
 
 resource "aws_subnet" "prod_subnet" {
   vpc_id     = aws_vpc.prod.id
   cidr_block = var.prod_subnet_cidr
+
+  tags = {
+    Name = "prod_subnet"
+  }
 }
 
 resource "aws_instance" "prod_instance" {
